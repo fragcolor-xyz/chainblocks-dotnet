2a1e36b74a77ce90dd705e5c64cff0c9e3ffdf9b
Nicolas Musset <musset.nicolas@gmail.com>
2022-01-04 21:40:24 +0100

    Update Godot sample

---
+++ Godot/Scripts/ChainblocksExtensions.cs
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright © 2022 Fragcolor Pte. Ltd. */
+
+using System.Runtime.CompilerServices;
+
+using Fragcolor.Chainblocks;
+
+using Godot;
+
+internal static class ChainblocksExtensions
+{
+    public static Float2 ToFloat2(this ref Vector2 vector)
+    {
+        return new Float2 { x = vector.x, y = vector.y };
+    }
+
+    public static ref Float3 ToFloat3(this ref Vector3 vector)
+    {
+        return ref Unsafe.As<Vector3, Float3>(ref vector);
+    }
+
+    public static Vector2 ToVector2(this ref Float2 @float)
+    {
+        return new Vector2((float)@float.x, (float)@float.y);
+    }
+
+    public static ref Vector3 ToVector3(this ref Float3 @float)
+    {
+        return ref Unsafe.As<Float3, Vector3>(ref @float);
+    }
+}
