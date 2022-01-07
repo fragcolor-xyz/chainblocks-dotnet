2071539ec28177c038e5266cc59af4825a62ff63
Nicolas Musset <musset.nicolas@gmail.com>
2022-01-07 18:17:21 +0100

    Thread-safe initialization of Native.Core

--- src/Chainblocks.Common/Native.cs
+++ src/Chainblocks.Common/Native.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Chainblocks
 {
   public static class Native
   {
-    private static IntPtr _core;
-    private static CBCore _coreCopy;
+    private static CBCore _core;
+    private static IntPtr _corePtr;
+
+    private static readonly object _syncLock = new();
 
     public static ref CBCore Core
     {
       get
       {
-        if (_core == IntPtr.Zero)
+        if (Volatile.Read(ref _corePtr) != IntPtr.Zero)
         {
-          _core = NativeMethods.chainblocksInterface(0x20200101);
-          _coreCopy = Marshal.PtrToStructure<CBCore>(_core);
+          return ref _core;
         }
-        return ref _coreCopy;
+
+        lock (_syncLock)
+        {
+          if (Volatile.Read(ref _corePtr) == IntPtr.Zero)
+          {
+            var ptr = NativeMethods.chainblocksInterface(0x20200101);
+            _core = Marshal.PtrToStructure<CBCore>(ptr);
+            Volatile.Write(ref _corePtr, ptr);
+          }
+        }
+
+        return ref _core;
       }
     }
   }
